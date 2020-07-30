19979bde114252a34b86e48b39589e5a04dd34ff
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 12:29:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length > 2
+  array[-1] = "and #{array[-1]}"
+  array.join(", ")
+elsif array.length == 2
+  array[-1] = "and #{array[-1]}"
+  array.join (" ")
+else
+  array.join
+end
 end
