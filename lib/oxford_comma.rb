3ea92941bd199bd4f3ceaa2e7acd17c564e46e9d
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 12:16:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
 require 'pry'
 
-nums = (1..10).to_a
-puts nums
 
-# def oxford_comma(array)
-  # array[-1] = "and #{array[-1]}"
-  # puts array
 
-  # binding.pry
-# end
+def oxford_comma(array)
+  array[-1] = "and #{array[-1]}"
+  nums = (1..10).to_a
+  binding.pry
+end
 
 
 # oxford_comma
