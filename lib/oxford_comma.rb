a02ad57dc270a1b64d9b2b09d656e34e0800cc9c
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 12:15:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,12 @@ include 'pry'
 nums = (1..10).to_a
 puts nums
 
-def oxford_comma(array)
+# def oxford_comma(array)
   # array[-1] = "and #{array[-1]}"
   # puts array
 
   # binding.pry
-end
+# end
 
 
 # oxford_comma
